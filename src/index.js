61b505b334f3de19aaf5097ef80733c02acd58d3
roqurdl <103472276+roqurdl@users.noreply.github.com>
2022-05-18 15:58:20 +0900

    Check2 계산의 단위오류 변경

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ const CAL_DAY = 1000 * 60 * 60 * 24;
 const CAL_HOUR = 1000 * 60 * 60;
 const CAL_MIN = 1000 * 60;
 
+const currentYear = new Date().getFullYear();
+
 function paintDDay() {
-  const currentYear = new Date().getFullYear();
   const dDay = new Date(`December 25, ${currentYear}`);
   const today = new Date();
   const gap = dDay.getTime() - today.getTime();
-  const day = Math.ceil(gap / CAL_DAY);
-  const hour = Math.ceil((gap % CAL_DAY) / CAL_HOUR);
-  const min = Math.ceil((gap % CAL_HOUR) / CAL_MIN);
-  const sec = Math.ceil((gap % CAL_MIN) / 1000);
+  const day = Math.floor(gap / CAL_DAY);
+  const hour = Math.floor((gap % CAL_DAY) / CAL_HOUR);
+  const min = Math.floor((gap % CAL_HOUR) / CAL_MIN);
+  const sec = Math.floor((gap % CAL_MIN) / 1000);
 
   clockTitle.innerText = `${day}d ${hour}h ${min}m ${sec}s`;
 }
